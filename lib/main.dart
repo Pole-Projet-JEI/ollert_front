7fa702d314a914a3aa2b82243385db77f59f86cc
Nour Kalai <nourkalai@insat.u-carthage.tn>
2021-12-21 11:18:24 +0100

    UI Consult Project added

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:ollert/screens/signIn.dart';
 import 'package:ollert/screens/signOut.dart';
 void main() {
-
   runApp( MyApp());
 }
 
 class MyApp extends StatefulWidget {
-
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -23,7 +20,7 @@ class _MyAppState extends State<MyApp> {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: LogoutPage(),
 
     );
   }
